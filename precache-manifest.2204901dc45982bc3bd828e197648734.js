6660c388f28e6bc97a0ad91e05fe0af09bdbff30
Björn O <bjorn.ohlsson.93@gmail.com>
2023-05-09 23:14:39 +0200

    Updates

---
+++ precache-manifest.2204901dc45982bc3bd828e197648734.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa92cebc295eb3ba4e88f457219e09b9",
+    "url": "/ketchupkungen/receptbanken.git/index.html"
+  },
+  {
+    "revision": "cd8a31b1766965371ded",
+    "url": "/ketchupkungen/receptbanken.git/static/css/main.0e016405.chunk.css"
+  },
+  {
+    "revision": "5399e4840327edf52ad9",
+    "url": "/ketchupkungen/receptbanken.git/static/js/2.017e1a51.chunk.js"
+  },
+  {
+    "revision": "b7eccec251554a44797082ce4436f9a8",
+    "url": "/ketchupkungen/receptbanken.git/static/js/2.017e1a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8a31b1766965371ded",
+    "url": "/ketchupkungen/receptbanken.git/static/js/main.d2355e7a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/ketchupkungen/receptbanken.git/static/js/main.d2355e7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024b1d1f6a27694356e4",
+    "url": "/ketchupkungen/receptbanken.git/static/js/runtime-main.0f69fe2f.js"
+  },
+  {
+    "revision": "d58b9b65a47dbff849352db445679bff",
+    "url": "/ketchupkungen/receptbanken.git/static/media/background.d58b9b65.jpg"
+  },
+  {
+    "revision": "477ee4649f8767d0315634204f0ddef7",
+    "url": "/ketchupkungen/receptbanken.git/static/media/logo.477ee464.png"
+  }
+]);
